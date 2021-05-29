630ca869158d07b97eaf5d2a9dea0ca47854c0c2
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-05-29 12:16:01 +0600

    moment.js used to format order-date

--- models/order.js
+++ models/order.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 class Order {
   constructor(orderId, orderItems, orderTotalAmount, orderDate) {
     this.orderId = orderId;
@@ -7,13 +9,14 @@ class Order {
   }
   get ReadableDate() {
     // convert into readble date
-    return this.orderDate.toLocaleDateString("en-EN", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
+    // return this.orderDate.toLocaleDateString("en-EN", {
+    //   year: "numeric",
+    //   month: "long",
+    //   day: "numeric",
+    //   hour: "2-digit",
+    //   minute: "2-digit",
+    // });
+    return moment(this.orderDate).format("MMMM Do YYYY, hh:mm");
   }
 }
 
