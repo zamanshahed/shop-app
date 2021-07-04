1e91cc19a0d6b2bb5cefaf6222bb78712fe7fa87
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-07-04 14:56:03 +0600

    confirmed-orders are now syncing to firebase server under fake-user 'u1'

--- store-redux/actions/Order.js
+++ store-redux/actions/Order.js
@@ -1,8 +1,37 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { item: cartItems, amount: totalAmount },
+  return async (dispatch) => {
+    const date = new Date();
+    const response = await fetch(
+      "https://rn-shop-app-40f1c-default-rtdb.firebaseio.com/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("something went wrong, Response not ok");
+    }
+
+    const resData = await response.json(); //async task
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        item: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
